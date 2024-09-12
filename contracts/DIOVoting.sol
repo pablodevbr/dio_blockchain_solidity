30a459774dd59211ca7f41f374ecb66c0e6bc71b
pablodevbr <pablodevbr@gmail.com>
2024-09-12 19:52:10 -0300

    primeiro commit

---
+++ contracts/DIOVoting.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract DIOVoting {
+    mapping (string => uint256) public votesReceived;
+
+    string[] public candidateList;
+
+    constructor (string[] memory candidateNames){
+        candidateList = candidateNames;
+    }
+
+    function totalVotesFor(string memory candidate) view public returns (uint256) {
+        require(validCandidate(candidate));
+
+        return votesReceived[candidate];
+    }
+
+    function votesForCandidate(string memory candidate) public {
+        require(validCandidate(candidate));
+        votesReceived[candidate]++;
+    }
+
+    function validCandidate(string memory candidate) view public returns(bool) {
+        for(uint i=0;i<candidateList.length;i++){
+            if(keccak256(abi.encodePacked(candidateList[i])) == keccak256(abi.encodePacked(candidate))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
